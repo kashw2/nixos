16ce4f9fe0df0aeca1de37b5c006deedf0049571
kashw2 <supra4keanu@hotmail.com>
2026-04-14 07:44:53 +1000

    feat(nix): move to isolated directory

---
+++ modules/features/nix/default.nix
@@ -0,0 +1,50 @@
+{ self, inputs, ... }:
+{
+  flake.nixosModules.nix =
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    {
+
+      nixpkgs.config = {
+        allowUnfree = true;
+        nvidia.acceptLicense = builtins.elem "nvidia" config.services.xserver.videoDrivers;
+      };
+
+      nix = {
+        package = inputs.nix.packages.${pkgs.stdenv.hostPlatform.system}.nix;
+        optimise.automatic = true;
+        gc.automatic = true;
+        channel.enable = false; # All hosts use flakes
+        extraOptions = ''
+          warn-dirty = false
+          !include ${config.sops.templates."nix-access-tokens".path}
+        '';
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          substituters = [
+            "https://cache.nixos.org/"
+          ];
+          trusted-substituters = [
+            "https://cache.nixos.org/"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          ];
+          auto-optimise-store = true;
+          trusted-users = [
+            "keanu"
+            "kashw2"
+            "@wheel"
+          ];
+        };
+      };
+
+    };
+}
