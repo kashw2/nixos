b844db99bbaacffe66904b67a3ac91ecbd1e0a20
kashw2 <supra4keanu@hotmail.com>
2026-04-17 10:24:09 +1000

    chore(quickshell): remove quickshell input

--- flake.nix
+++ flake.nix
@@ -47,10 +47,6 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
--- modules/features/desktop-environment/quickshell.nix
+++ modules/features/desktop-environment/quickshell.nix
@@ -21,7 +21,7 @@
         };
       };
       config = {
-        package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
+        package = pkgs.quickshell;
         extraPackages = [
           pkgs.networkmanager
           pkgs.bluez
