217e7a7a71f01cbe5d2e7a24ec46cf71b2ade2f0
kashw2 <supra4keanu@hotmail.com>
2026-05-05 21:50:50 +1000

    chore(telemetry): collapse basic to default.nix

--- modules/features/telemetry/default.nix
+++ modules/features/telemetry/default.nix
@@ -1,11 +1,14 @@
 { self, inputs, ... }:
 {
   flake.nixosModules.telemetry =
-    { pkgs, lib, ... }:
+    {
+      config,
+      lib,
+      ...
+    }:
     {
 
       imports = [
-        self.nixosModules.basicTelemetry
         self.nixosModules.alloy
         self.nixosModules.loki
         self.nixosModules.grafana
@@ -20,6 +23,42 @@
         ];
         default = "client";
         description = "Whether this machine is a telemetry host (runs the LGTM stack) or a client (only exports metrics).";
+      };
+
+      config = {
+        networking.firewall.allowedTCPPorts =
+          [ ]
+          ++ lib.optionals (config.services.prometheus.exporters.node.enable) [
+            config.services.prometheus.exporters.node.port
+          ]
+          ++ lib.optionals (config.services.prometheus.exporters.nvidia-gpu.enable) [
+            config.services.prometheus.exporters.nvidia-gpu.port
+          ];
+
+        services = {
+          rsyslogd = {
+            enable = true;
+            extraConfig = ''
+              auth,authpriv.*              -/var/log/auth.log
+              kern.*                       -/var/log/kernel.log
+              cron.*                       -/var/log/cron.log
+              user.*                       -/var/log/user.log
+            '';
+          };
+          prometheus = {
+            exporters = {
+              node = {
+                enable = true;
+                enabledCollectors = [ "systemd" ];
+                port = 9002;
+              };
+              nvidia-gpu = {
+                enable = builtins.elem "nvidia" config.services.xserver.videoDrivers;
+                port = 9835;
+              };
+            };
+          };
+        };
       };
 
     };
