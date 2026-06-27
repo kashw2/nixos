2df88eaaf2f98e509aa3cf5cbdf8ad038c039b33
kashw2 <15855440+kashw2@users.noreply.github.com>
2026-06-27 21:09:40 +1000

    fix(nixvim:keymaps): fix tq tabclose, gate db on !isServer, add silent
    - tq was missing <CR> so :tabclose never executed
    - db (DBUIToggle) was always registered even on servers where dadbod is
      disabled; moved into lib.optionals (!config.isServer)
    - Added options.silent = true to all :command<CR> keymaps to stop them
      flashing through the cmdline on every press
    - Added config to module args to support the isServer conditional

--- modules/features/nixvim/keymaps.nix
+++ modules/features/nixvim/keymaps.nix
@@ -1,7 +1,12 @@
 { self, inputs, ... }:
 {
   flake.nixosModules.nixvimKeymaps =
-    { pkgs, lib, ... }:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
     {
       programs.nixvim.keymaps = [
         # Unmap
@@ -26,16 +31,19 @@
           action = ":w<CR>";
           key = "<C-s>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":undo<CR>";
           key = "<C-z>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":redo<CR>";
           key = "<C-S-z>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = "yy";
@@ -60,12 +68,16 @@
           action = ":lua require('fastaction').code_action()<CR>";
           key = "ca";
           mode = [ "n" ];
-          options.remap = true;
+          options = {
+            remap = true;
+            silent = true;
+          };
         }
         {
           action = ":BlameToggle virtual<CR>";
           key = "gb";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action.__raw = ''
@@ -85,123 +97,150 @@
         }
         # Tabs
         {
-          action = ":tabclose";
+          action = ":tabclose<CR>";
           key = "tq";
           mode = [ "n" ];
+          options.silent = true;
         }
         # Splits
         {
           action = ":vsplit<CR>";
           key = "sr";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":split<CR>";
           key = "sd";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":q<CR>";
           key = "sq";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":SmartResizeUp<CR>";
           key = "<A-Up>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":SmartResizeRight<CR>";
           key = "<A-Right>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":SmartResizeDown<CR>";
           key = "<A-Down>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":SmartResizeLeft<CR>";
           key = "<A-Left>";
           mode = [ "n" ];
+          options.silent = true;
         }
         # Atone
         {
           action = ":Atone toggle<CR>";
           key = "ut";
           mode = [ "n" ];
+          options.silent = true;
         }
         # ToggleTerm
         {
           action = ":TermNew direction=float<CR>";
           key = "tn";
           mode = [ "n" ];
+          options.silent = true;
         }
         # LazyGit
         {
           action = ":LazyGit<CR>";
           key = "gui";
           mode = [ "n" ];
+          options.silent = true;
         }
         # Buffers
         {
           action = ":Bdelete!<CR>";
           key = "bq";
           mode = [ "n" ];
+          options.silent = true;
         }
         # Navigation
         {
           action = ":wincmd k<CR>";
           key = "<C-Up>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":wincmd l<CR>";
           key = "<C-Right>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":wincmd j<CR>";
           key = "<C-Down>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":wincmd h<CR>";
           key = "<C-Left>";
           mode = [ "n" ];
+          options.silent = true;
         }
         # NvimTree
         {
           action = ":NvimTreeToggle<CR>";
           key = "<C-d>";
           mode = [ "n" ];
+          options.silent = true;
         }
         # Telescope
         {
           action = ":Telescope current_buffer_fuzzy_find<CR>";
           key = "<C-f>";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":Telescope live_grep<CR>";
           key = "fg";
           mode = [ "n" ];
-          options.remap = true;
+          options = {
+            remap = true;
+            silent = true;
+          };
         }
         {
           action = ":Telescope buffers<CR>";
           key = "fb";
           mode = [ "n" ];
+          options.silent = true;
         }
         {
           action = ":Telescope find_files<CR>";
           key = "ff";
           mode = [ "n" ];
+          options.silent = true;
         }
+      ]
+      ++ lib.optionals (!config.isServer) [
+        # Database
         {
           action = ":DBUIToggle<CR>";
           key = "db";
           mode = [ "n" ];
+          options.silent = true;
         }
       ];
     };
