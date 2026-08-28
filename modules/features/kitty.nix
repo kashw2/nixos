71895aef4ad5d1cbcab814c41382097bf54d67f1
kashw2 <supra4keanu@hotmail.com>
2026-08-28 15:38:12 +1000

    fix(kitty): forward modified keys to apps through tmux

--- modules/features/kitty.nix
+++ modules/features/kitty.nix
@@ -26,6 +26,17 @@
           cursor_shape beam
           cursor_trail 1
           cursor_trail_decay 0.1 0.6
+          map ctrl+, send_text all \x1b[44;5u
+          map ctrl+. send_text all \x1b[46;5u
+          map ctrl+; send_text all \x1b[59;5u
+          map ctrl+' send_text all \x1b[39;5u
+          map ctrl+/ send_text all \x1b[47;5u
+          map ctrl+minus send_text all \x1b[45;5u
+          map ctrl+equal send_text all \x1b[61;5u
+          map ctrl+grave_accent send_text all \x1b[96;5u
+          map ctrl+shift+left send_text all \x1b[1;6D
+          map ctrl+shift+right send_text all \x1b[1;6C
+          map shift+enter send_text all \x1b[13;2u
         ''}/kitty.conf";
       };
     };
